579e920f17365910f2ed17d1b5557d765600979d
hasparus <hasparus@gmail.com>
2020-02-22 16:18:58 +0100

    annotate funcs ¯\_(ツ)_/¯

--- packages/components/src/Message.tsx
+++ packages/components/src/Message.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Box, { BoxProps } from './Box'
 import { ForwardRef } from './types'
+import { Theme } from '@theme-ui/css'
 
 export type MessageProps = BoxProps
 
@@ -20,8 +21,8 @@ export const Message: ForwardRef<
     __themeKey="messages"
     css={{
       padding: 3,
-      paddingLeft: t => t.space[3] - t.space[1], //TODO
-      borderLeftWidth: t => t.space[1], //TODO
+      paddingLeft: (t: Theme) => t.space[3] - t.space[1], //TODO
+      borderLeftWidth: (t: Theme) => t.space[1], //TODO
       borderLeftStyle: 'solid',
       borderLeftColor: 'primary',
       borderRadius: 4,
