f208bd116212c66889d725a720c57fb4512e0f41
hasparus <hasparus@gmail.com>
2020-02-22 14:08:37 +0100

    refactor(@theme-ui/components): type src/util

--- packages/components/src/util.ts
+++ packages/components/src/util.ts
@@ -1,5 +1,7 @@
-export const getProps = test => props => {
-  const next = {}
+export const getProps = (test: (s: string) => any) => (
+  props: Record<string, any>
+) => {
+  const next: Record<string, any> = {}
   for (const key in props) {
     if (test(key || '')) next[key] = props[key]
   }
